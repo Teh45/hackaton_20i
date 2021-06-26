75235ffb7606d3c5ace46a5405926d15af9c9615
Teh45 <theohubert3@gmx.com>
2021-06-26 21:04:00 +0200

    Django PostGreSQL setup

--- StudyFun/front/urls.py
+++ StudyFun/front/urls.py
@@ -6,5 +6,6 @@ from . import views # import views so we can use them in urls.
 urlpatterns = [
     url(r'^$', views.index),
     path('home/', views.index, name="home"),
+    path('settings/', views.settingsPage, name="settings"),
     path('help/', views.helpPage, name="help"),
 ]
