dfb043ef88ec04a879894d4515dd2c8c13b558c2
Teh45 <theohubert3@gmx.com>
2021-06-27 02:53:54 +0200

    Front end design 1

--- StudyFun/front/views.py
+++ StudyFun/front/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    # Charging last input
+
     return render(request, 'front/page_home.html')
 
 def helpPage(request):
-    # Charging last input
+
     return render(request,'front/page_help.html')
 
 def settingsPage(request):
-    # Charging last input
-    result = 2
-    return render(request, 'front/page_settings.html', {'result': result})
+
+    return render(request, 'front/page_settings.html')
+
+def statisticsPage(request):
+    return render(request, 'front/page_statistics.html')
